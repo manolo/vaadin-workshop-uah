97bda8e5b24089d2218f55a8a191af550dd0f183
Manolo Carrasco <manolo@vaadin.com>
2016-02-16 23:35:53 +0200

    Step10 - Improved UX

--- src/main/java/com/example/ui/WorkshopUI.java
+++ src/main/java/com/example/ui/WorkshopUI.java
@@ -6,9 +6,12 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.util.BeanItem;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
@@ -20,27 +23,51 @@ public class WorkshopUI extends UI implements FieldGroup.CommitHandler {
 
     private BeanItemContainer<Customer> container = new BeanItemContainer<>(Customer.class);
 
+    private TextField textField;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         TabSheet tabs = new TabSheet();
-        tabs.addTab(buildGreetingComponent(), "Greeting");
-        tabs.addTab(buildAdminComponent(), "Admin");
+        tabs.setSizeFull();
+        tabs.addStyleName(ValoTheme.TABSHEET_FRAMED);
+        tabs.addStyleName(ValoTheme.TABSHEET_EQUAL_WIDTH_TABS);
+        tabs.addStyleName(ValoTheme.TABSHEET_PADDED_TABBAR);
+        tabs.addTab(buildGreetingComponent(), "Greeting", FontAwesome.COMMENTS);
+        tabs.addTab(buildAdminComponent(), "Admin", FontAwesome.USERS);
         setContent(tabs);
+
+        textField.focus();
 
         updateContainer();
     }
 
     private Component buildGreetingComponent() {
-        TextField textField = new TextField("Name");
+        textField = new TextField("Name");
+        textField.setNullRepresentation("");
 
-        Button button = new Button("Greet");
+        Button button = new Button("Greet", FontAwesome.COMMENT);
+        button.addStyleName(ValoTheme.BUTTON_PRIMARY);
         button.addClickListener(e -> greet(textField.getValue()));
+        button.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 
-        return new VerticalLayout(textField, button);
+        VerticalLayout panelContent = new VerticalLayout(textField, button);
+        panelContent.setMargin(true);
+        panelContent.setSpacing(true);
+
+        Panel panel = new Panel("Greeting form", panelContent);
+        panel.setSizeUndefined();
+
+        VerticalLayout layout = new VerticalLayout(panel);
+        layout.setSizeFull();
+        layout.setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
+
+        return layout;
     }
 
     private void greet(String name) {
-        Notification.show(greetingService.greet(name));
+        Notification.show("Greetings!", greetingService.greet(name), Notification.Type.TRAY_NOTIFICATION);
+        textField.setValue("");
+        textField.focus();
         updateContainer();
     }
 
@@ -50,7 +77,11 @@ public class WorkshopUI extends UI implements FieldGroup.CommitHandler {
         grid.setEditorEnabled(true);
         grid.getColumn("id").setEditable(false);
         grid.getEditorFieldGroup().addCommitHandler(this);
-        return grid;
+
+        VerticalLayout layout = new VerticalLayout(grid);
+        layout.setMargin(true);
+
+        return layout;
     }
 
     private void updateContainer() {
