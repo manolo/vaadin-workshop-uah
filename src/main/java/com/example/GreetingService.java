c00c4cb3d799dbb70f00bcef40ed5676a319d839
Manolo Carrasco <manolo@vaadin.com>
2016-02-16 20:18:06 +0200

    Step4 - Implemented JPA entity, repository, gretting logic.

--- src/main/java/com/example/GreetingService.java
+++ src/main/java/com/example/GreetingService.java
@@ -1,12 +1,30 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     public String greet(String name) {
-        return "Hi " + name;
+
+        Customer existingCustomer = customerRepository.getByName(name);
+
+        if (existingCustomer != null) {
+            return "Hi " + name + ", welcome back!";
+
+        } else {
+            Customer newCustomer = new Customer();
+            newCustomer.setName(name);
+
+            customerRepository.save(newCustomer);
+
+            return "Hi " + name;
+        }
+
     }
 
 }
